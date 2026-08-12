6b7749153df99f3d608470686cf3dfd1b9dd2528
Rommerfy <165468818+MaddIsHere@users.noreply.github.com>
2026-08-12 19:23:24 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Device
-export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
-export TWRP_BRANCH="twrp-12.1"
-#export TWRP_BRANCH="twrp-test"
-export DT_LINK="https://github.com/faoliveira78/android_device_oneplus_cheeseburger_dumpling -b android-12.1"
+export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
+export TWRP_BRANCH="twrp-9.0"
 
-export DEVICE="cheeseburger_dumpling"
-export OEM="oneplus"
-#export PLATFORM="msm8998"
+export DT_LINK="https://github.com/MaddIsHere/twrp_android_device_samsung_jadeltechn -b master"
+
+export DEVICE="jadeltechn"
+export OEM="samsung"
 export TARGET="recoveryimage"
-export TW_DEVICE_VERSION="4.14.1-dyn"
 export OUTPUT="recovery.img"
 export FILENAME="twrp*.img"
-
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
 #export KERNEL_SOURCE="https://github.com/faoliveira78/android_kernel_oneplus_msm8998"
